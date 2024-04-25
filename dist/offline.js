7b04f491b404eba2b4c5a35cc99a8f6edc3dfb42
Lucas Rayan <53474519+Spishous@users.noreply.github.com>
2024-04-26 00:50:53 +0200

    Add files via upload

---
+++ dist/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1714085342,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/mainbg-sheet0.png",
+		"images/veonum-sheet0.png",
+		"images/title-sheet0.png",
+		"images/pressstart.png",
+		"images/fader-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite5-sheet1.png",
+		"images/flame-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
